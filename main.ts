0fb0c27c1b0629968be6ade4621e7fd270a7e297
yuukim8 <91284065+yuukim8@users.noreply.github.com>
2021-09-24 10:48:32 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,53 @@
-
+let projectile2: Sprite = null
+let Dino = sprites.create(img`
+    ........................
+    ............cc..........
+    ............ccc.........
+    ........ccc.ccccccc.....
+    ........ccccc555555cc...
+    ........ccb5555555555c..
+    .....ccc.b55555ff15555c.
+    .....cccb5555555ff55555c
+    ......cb555555555555d55c
+    ....c.b555555555bb55555c
+    ....ccb555ddd5555b13bbc.
+    ....ccd55ddddd555b3335c.
+    .....cdd5ddddddd55b335c.
+    ...c.bddddb555bbbd555c..
+    ...ccdddddbb55555bccc...
+    ...ccdddddddcc555bcc....
+    ...ccddddddddbcccbcccc..
+    .ccbddddddd55dbbbbc55c..
+    ccddddddddd555dbbcc5c...
+    cddddddbbbdd555bbccc....
+    .ccddddbbbbdd55bcc......
+    ...cccbbbbbdddbcddcc....
+    .....cccccdd555dcccc....
+    ..........cccccc........
+    `, SpriteKind.Player)
+scene.setBackgroundColor(7)
+controller.moveSprite(Dino)
+Dino.setStayInScreen(true)
+info.setScore(0)
+info.startCountdown(10)
+game.onUpdateInterval(500, function () {
+    projectile2 = sprites.createProjectileFromSide(img`
+        . . 2 2 b b b b b . . . . . . . 
+        . 2 b 4 4 4 4 4 4 b . . . . . . 
+        2 2 4 4 4 4 d d 4 4 b . . . . . 
+        2 b 4 4 4 4 4 4 d 4 b . . . . . 
+        2 b 4 4 4 4 4 4 4 d 4 b . . . . 
+        2 b 4 4 4 4 4 4 4 4 4 b . . . . 
+        2 b 4 4 4 4 4 4 4 4 4 e . . . . 
+        2 2 b 4 4 4 4 4 4 4 b e . . . . 
+        . 2 b b b 4 4 4 b b b e . . . . 
+        . . e b b b b b b b e e . . . . 
+        . . . e e b 4 4 b e e e b . . . 
+        . . . . . e e e e e e b d b b . 
+        . . . . . . . . . . . b 1 1 1 b 
+        . . . . . . . . . . . c 1 d d b 
+        . . . . . . . . . . . c 1 b c . 
+        . . . . . . . . . . . . c c . . 
+        `, 50, 0)
+    projectile2.setPosition(0, 0)
+})
